d33061e4d411236923b4a015f5b97180c009d1de
kame-turtle <60323549+kame-turtle@users.noreply.github.com>
2020-10-29 02:36:05 +0900

    Add swiper

--- scripts/librarys/swiper.js
+++ scripts/librarys/swiper.js
@@ -2,10 +2,12 @@
 
 let mySwiper = new Swiper('.swiper-container', {
   // 以下,オプション設定
-  loop: true, //最後に達したら先頭に戻る
-
-  slidesPerView:2,  //スライドを2つ（分）表示
-  centeredSlides : true,  //アクティブなスライドを中央に表示
+  loop: true,   //最後に達したら先頭に戻る
+  speed: 1000,  //スライド遷移にかける時間(ms)
+  autoplay: {   //自動再生したい場合の基本設定
+    delay: 1500,  //スライド切替間隔(ms)
+    disableOnInteraction: true  //クリック、ドラッグされたら自動再生を無効にする
+  },
   effect: 'coverflow',  //スライドのエフェクトを coverflow に
 
   //ページネーション表示の設定
@@ -25,6 +27,19 @@ let mySwiper = new Swiper('.swiper-container', {
   scrollbar: {
     el: '.swiper-scrollbar', //要素の指定
   },
+
+  slidesPerView:1,  //スライドを2つ（分）表示
+  centeredSlides : true,  //アクティブなスライドを中央に表示
+  breakpoints: {
+    // 768px以上の場合
+    768: {
+      slidesPerView: 1,
+    },
+    // 980px以上の場合
+    980: {
+      slidesPerView: 2,
+    },
+  }
 })
 
 console.log('test')
